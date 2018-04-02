695d87ca48c2f3d2baee96014f48770552baf7de
Mxtzz <mazw2016@hotmail.com>
2018-04-02 10:49:04 +0800

    构建主页面

--- 快知家/app.js
+++ 快知家/app.js
@@ -22,6 +22,11 @@ App({
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
 
+              //登陆后跳转至tabs页面
+              wx.switchTab({
+                url: '../home/home'
+              })
+
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
--- 快知家/pages/message/message.js
+++ 快知家/pages/message/message.js
@@ -1,86 +1,66 @@
-
+// pages/message/message.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    title : "Hello World",
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      '../img/scroll/scroll01.jpg',
-      '../img/scroll/scroll02.jpg',
-      '../img/scroll/scroll03.jpg',
-      '../img/scroll/scroll04.jpg',
-      '../img/scroll/scroll05.jpg',
-    ],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 3000,
-    duration: 1000,
-    // indicator_dots:true,
-  },
-
   
-
-  clickMe:function(){
-    this.setData({msg:"Hello World"})
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+  
   }
 })
